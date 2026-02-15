1cb1b20791cfb7994fa7e2053ca96fd15077625e
bayezsyka <farrosy6@gmail.com>
2026-02-15 16:40:26 +0700

    feat: Implement initial application routing, user authentication, and PWA development proxy.

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,7 @@ use Inertia\Inertia;
 use Illuminate\Support\Facades\Http;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return redirect()->route('nursery.index');
 });
 
 Route::get('/dashboard', function () {
